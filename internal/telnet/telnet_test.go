225e419624c08d287f4639585d49cddd4f1915c3
Samantha Tesla <stesla@pobox.com>
2025-07-17 18:13:10 -0500

    rework read test to test split inputs

--- internal/telnet/telnet_test.go
+++ internal/telnet/telnet_test.go
@@ -25,47 +25,26 @@ func (m *mockConn) Write(b []byte) (n int, err error)  { return m.wbuf.Write(b) 
 
 // TODO: test Read contract parameters
 
-func TestReadSimple(t *testing.T) {
-	var tests = []struct {
-		val, expected []byte
-	}{
-		{[]byte("foo"), []byte("foo")},
-		{[]byte{'h', IAC, NOP, 'i'}, []byte("hi")},
-		{[]byte{'h', IAC, IAC, 'i'}, []byte{'h', IAC, 'i'}},
-		{[]byte("foo\r\nbar"), []byte("foo\nbar")},
-		{[]byte("foo\r\x00bar"), []byte("foo\rbar")},
-		{[]byte{'h', IAC, SB, Echo, IAC, SE, 'i'}, []byte("hi")},
-		{
-			func() (result []byte) {
-				for c := range byte(127) {
-					result = append(result, '\r', c)
-				}
-				return
-			}(),
-			[]byte("\r\n"),
-		},
-	}
-	for _, test := range tests {
-		tcp := &mockConn{}
-		tcp.rbuf.Write(test.val)
-		telnet := Wrap(tcp)
-		actual := make([]byte, len(test.val))
-		n, err := telnet.Read(actual)
-		require.NoError(t, err)
-		require.Equal(t, test.expected, actual[:n])
-	}
-}
-
-func TestSplitInput(t *testing.T) {
+func TestRead(t *testing.T) {
 	var tests = []struct {
 		vals     [][]byte
 		expected []byte
 	}{
+		{[][]byte{[]byte("foo")}, []byte("foo")},
 		{[][]byte{{'h', IAC}, {NOP, 'i'}}, []byte("hi")},
 		{[][]byte{{'h', IAC}, {IAC, 'i'}}, []byte{'h', IAC, 'i'}},
 		{[][]byte{[]byte("foo\r"), []byte("\nbar")}, []byte("foo\nbar")},
 		{[][]byte{[]byte("foo\r"), []byte("\x00bar")}, []byte("foo\rbar")},
 		{[][]byte{{'h', IAC, SB}, {Echo, IAC}, {SE, 'i'}}, []byte("hi")},
+		{
+			func() (result [][]byte) {
+				for c := range byte(127) {
+					result = append(result, []byte{'\r', c})
+				}
+				return
+			}(),
+			[]byte("\r\n"),
+		},
 	}
 	const bufsize = 16
 	for _, test := range tests {
@@ -83,7 +62,7 @@ func TestSplitInput(t *testing.T) {
 	}
 }
 
-func TestWriteSimple(t *testing.T) {
+func TestWrite(t *testing.T) {
 	var tests = []struct {
 		val, expected []byte
 	}{
